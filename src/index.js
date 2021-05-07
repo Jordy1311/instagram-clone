80f4bba3db100ac2b7bb39d1499e5ca4afe8babd
Jordy1311 <jordancooper1195@gmail.com>
2021-05-07 13:44:18 +1200

    added seed.js and created folder structure

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// CLIENT SIDE RENDERED APP: react (cra)
+  // database which is Firebase
+  // react-loading-skeleton
+  // tailwind
+
+// FOLDER STRUCTURE
+  // src
+    // components, 
+    // constants (roots), 
+    // context, 
+    // helpers, 
+    // lib - (firebase is going to live in here), 
+    // services - (firebase functions in here)
+    // styles (tailwind's folder (app/tailwind))
